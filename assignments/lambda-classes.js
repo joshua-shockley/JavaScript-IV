f60f1ccf6432fae0dfdcae713feb3716214bf3e9
Joshua-Shockley <thelumbashockley@gmail.com>
2019-07-25 19:55:22 -0500

    assignment completed

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -7,7 +7,7 @@ class Person {
             this.location = attributes.location
     }
     speak() {
-        return `Hello my name is ${this.name}, I am from ${this.location}`;
+        console.log(`Hello my name is ${this.name}, I am from ${this.location}`);
     }
 }
 
@@ -20,15 +20,15 @@ class Instructor extends Person {
             this.catchPhrase = attributes.catchPhrase
     }
     demo(subject) {
-        return `Today we are learning about ${this.subject}`;
+        console.log(`Today we are learning about ${subject}`);
     }
-    grade(subject) {
-        return `${student.name} receives a perfect score on ${subject}`;
+    grade(student, subject) {
+        console.log(`${student.name} receives a perfect score on ${subject}`);
     }
 
 }
 
-class Student {
+class Student extends Person {
     constructor(attributes) {
         super(attributes);
         this.previousBackground = attributes.previousBackground,
@@ -36,7 +36,27 @@ class Student {
             this.favSubjects = attributes.favSubjects
     }
     listsSubjects() {
+        let subject = this.favSubjects.forEach(item =>
+            console.log(item));
+    }
+    PRAssignment(subject) {
+        console.log(`${this.name} has submitted a PR for ${subject}.`)
+    }
+}
 
+class ProjectManager extends Instructor {
+    constructor(attributes) {
+        super(attributes);
+        this.gradClassName = attributes.gradClassName,
+            this.favInstructor = attributes.favInstructor
+    }
+    standUp() {
+        let slackChannel = '{name}';
+        let channelName = '{channel}';
+        console.log(`${slackChannel} announces to ${channelName}, @channel standy times!`)
+    }
+    debugsCode(student, subject) {
+        console.log(`${this.name} debugs ${student.name}'s code on ${subject}`);
     }
 }
 
@@ -52,20 +72,56 @@ const instructorOne = new Instructor({
 const instructorTwo = new Instructor({
     name: 'the taughter',
     location: 'next room',
-    age: 55,
+    age: 35,
     favLanguage: 'C++',
     specialty: 'All of it',
     catchPhrase: 'Do or do not, there is not try'
 });
 
 const studentOne = new Student({
+    name: 'Malcom',
+    location: 'his room',
+    age: 15,
     previousBackground: 'server',
     className: 'web dev',
     favSubjects: ['HTML', 'CSS', 'JavaScript']
-})
+});
 
 const studentTwo = new Student({
+    name: 'Bobbie',
+    location: 'a room',
+    age: 16,
     previousBackground: 'retail',
     className: 'web dev',
-    favSubjects: ['HTML', 'CSS', 'JavaScript']
-})
+    favSubjects: ['JAVA', 'C++', 'JavaScript']
+});
+
+const projectManagerOne = new ProjectManager({
+    name: 'Manager Chiara',
+    location: 'library',
+    age: 75,
+    gradClassName: 'CS1',
+    favInstructor: 'Terry'
+});
+
+const projectManagerTwo = new ProjectManager({
+    name: 'Manager Dave',
+    location: 'here',
+    age: 65,
+    gradClassName: 'CS2',
+    favInstructor: 'Devon'
+});
+
+console.log(studentOne.name);
+console.log(studentTwo.className);
+console.log(projectManagerOne.favInstructor);
+console.log(projectManagerTwo.gradClassName);
+console.log(instructorOne.specialty);
+console.log(instructorTwo.catchPhrase);
+studentOne.listsSubjects();
+studentOne.PRAssignment('maths');
+instructorOne.grade(studentTwo, 'java');
+instructorTwo.demo('gym');
+projectManagerOne.speak();
+projectManagerOne.standUp();
+projectManagerTwo.debugsCode(studentOne, 'math');
